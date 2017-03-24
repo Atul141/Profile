b583fdebb2693871c787248b6dbf58e426182bcd
Atul Kumar <atulbk@thoughtworks.com>
2017-03-25 00:00:22 +0530

    Added tech images

--- src/SkillSet.js
+++ src/SkillSet.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
-import StarRating from 'react-star-rating'
+import StarRatingComponent from "react-star-rating-component";
 
 class SkillSet extends Component{
 
     render(){
         return(
-        <div>
-            <p>SkillSet</p>
-            <img src={require('../public/java.jpg')} alt={"java"} className="profile-image"/>
-            <StarRating name={"Skill Level"} rating={3.5} size={25}/>
+        <div >
+            <img src={require('../public/java.jpg')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+
+            <img src={require('../public/Spring.png')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+            <img src={require('../public/js.png')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+             <img src={require('../public/React.jpg')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+             <img src={require('../public/html.jpg')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+            <img src={require('../public/css.jpg')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+            <img src={require('../public/heroku.jpg')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
+            <img src={require('../public/postgresSql.jpg')} alt={"java"} className="skill-image"/>
+            <StarRatingComponent className="star-rating"
+                                        size={40}
+                                        editing={false}
+                                        emptyStarColor={"rgb(195,195,195)"}
+                                        value={3.5}
+                                    />
         </div>
         );
     }
