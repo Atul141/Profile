d0df2d75285d07863170638106a120b91c0db14b
Atul Kumar <atulbk@thoughtworks.com>
2017-03-27 20:32:05 +0530

    added skille set and contact content

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,22 @@ class Contact extends Component{
 
     render(){
         return(
-            <p>Contact</p>
+        <div>
+        <div>
+            <h3>Direct Contact Info</h3>
+            <div >
+                <p className="contact-text"><img src={require('../public/gmail.jpg')} alt={"java"} className="contact-image"/>     : atulkumar140@gmail.com</p>
+             </div>
+            <p>+91 9481437780</p>
+         </div>
+            <h3>View my contributions at</h3>
+           <i class="fa fa-stack-overflow" aria-hidden="true">stackoverflow.com/users/5855733/atul-kumar</i>
+            <p>github.com/Atul141</p>
+            <h3>Social Media</h3>
+            <p>facebook.com/atulbkumar140</p>
+            <p>linkedin.com/in/atul-kumar-659750b7</p>
+            <p>quora.com/profile/Atul-Kumar-195</p>
+            </div>
         );
     }
 }
--- src/Experience.js
+++ src/Experience.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import {Timeline, TimelineEvent} from 'react-event-timeline'
+
 
 class Experience extends Component{
 
     render(){
         return(
-            <p>Experience</p>
+        <div className="timeline">
+                <Timeline className="timeline">
+                        <TimelineEvent title="John Doe sent a SMS"
+                                       createdAt="2016-09-12 10:06 PM"
+                                       icon={<i className="material-icons md-18">textsms</i>}
+                       className="timeline" >
+                            </TimelineEvent>
+                        <TimelineEvent className="timeline"
+                            title="You sent an email to John Doe"
+                            createdAt="2016-09-11 09:06 AM"
+                            icon={<i className="material-icons md-18">email</i>}
+                        >
+
+                        </TimelineEvent>
+                </Timeline>,
+        </div>
         );
     }
 }
