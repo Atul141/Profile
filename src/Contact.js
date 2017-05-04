4a83d607276dca0285c9276eb71905ade6d9efd0
Atul Kumar <atulbk@thoughtworks.com>
2017-05-04 22:27:52 +0530

    Added images to contact page

--- src/Contact.js
+++ src/Contact.js
@@ -5,20 +5,16 @@ class Contact extends Component{
     render(){
         return(
         <div>
-        <div>
-            <h3>Direct Contact Info</h3>
-            <div >
-                <p className="contact-text"><img src={require('../public/gmail.jpg')} alt={"java"} className="contact-image"/>     : atulkumar140@gmail.com</p>
-             </div>
-            <p>+91 9481437780</p>
-         </div>
-            <h3>View my contributions at</h3>
-           <i class="fa fa-stack-overflow" aria-hidden="true">stackoverflow.com/users/5855733/atul-kumar</i>
-            <p>github.com/Atul141</p>
-            <h3>Social Media</h3>
-            <p>facebook.com/atulbkumar140</p>
-            <p>linkedin.com/in/atul-kumar-659750b7</p>
-            <p>quora.com/profile/Atul-Kumar-195</p>
+            <h3 className ="heading">Direct Contact Info</h3>
+                <p><img src={require('../public/gmail.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">atulkumar140@gmail.com</span></p>
+            <p><img src={require('../public/phone.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">+91 9481437780</span></p>
+            <h3  className ="heading">View my contributions at</h3>
+         <p><img src={require('../public/stack.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">stackoverflow.com/users/5855733/atul-kumar</span></p>
+            <p><img src={require('../public/github.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">github.com/Atul141</span></p>
+            <h3  className ="heading">Social Media</h3>
+            <p><img src={require('../public/facebook.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">facebook.com/atulbkumar140</span></p>
+            <p><img src={require('../public/linkedin.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">linkedin.com/in/atul-kumar-659750b7</span></p>
+            <p><img src={require('../public/quora.jpg')} alt={"java"} className="contact-image"/> <span className="contact-info">quora.com/profile/Atul-Kumar-195</span></p>
             </div>
         );
     }
